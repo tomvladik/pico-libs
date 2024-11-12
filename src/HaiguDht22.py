0b407d6cc21d7488cf08b80c0a46faec633b2620
Tomáš Vladík <tom.vladik@gmail.com>
2024-11-13 00:41:10 +0100

    addd separate measuremnt of temperature and humidity polling HumiA and HumiB just once after start

--- src/HaiguDht22.py
+++ src/HaiguDht22.py
@@ -15,7 +15,9 @@ class HAIGU_DHT22:
     # control constants
     I2C_ADDRESS = 0x44
 
-    CMD_MEASURE         = 0x2C10 # trigger measurement: temperature and hmidity at once
+    CMD_MEASURE              = 0x2C10 # trigger measurement: temperature and hmidity at once
+    CMD_MEASURE_TEMPERATURE  = 0xCC44 # trigger measurement: temperature
+    CMD_MEASURE_HUMIDITY     = 0xCC66 # trigger measurement: hmidity
     CMD_READ_STATUS     = 0xF32D # read status register
     CMD_CLEAR_STATUS    = 0x3041 # clear status register
     CMD_SOFT_RESET      = 0x30A2 # soft reset    
@@ -24,27 +26,38 @@ class HAIGU_DHT22:
     CMD_GET_HUMCOEFB_H  = 0xD20A # Read HumB high 8 bits  
     CMD_GET_HUMCOEFB_L  = 0xD20B # Read HumB lower 8 bits  
 
-    MEASUREMENT_WAIT_TIME = 0.020  
+    MEASUREMENT_WAIT_TIME = 0.050  
 
     def __init__(self, i2c, debug=False):
         self.debug = debug
+        self._temperature = None
         self.i2c = i2c
-        time.sleep_ms(50)
+        time.sleep(self.MEASUREMENT_WAIT_TIME)
+        self.soft_reset()
+        
+        while True:            
+            time.sleep(self.MEASUREMENT_WAIT_TIME)
+            self.humA, self.humB = self.get_humidity_coefficients()
+            if self.humA is not None and self.humB is not None:
+                break
        
     def soft_reset(self):
         self._write(self.CMD_SOFT_RESET)
 
-    def _get_coefficient(self, command):
+    def _get_byte_with_crc_check(self, command):
         self._write(command)
-        low_raw = self._read(3)
-        low, dummy_low, crc_low = struct.unpack('<BBB', low_raw)
+        raw_bytes = self._read(3)
+        byte, dummy, crc_received = struct.unpack('<BBB', raw_bytes)
+        crc_computed = self._calculate_checksum(byte<<8|dummy)
+        if crc_computed == crc_received:
+            return byte
+        return None
 
-        self._write(command + 1)
-        high_raw = self._read(3)
-        high, dummy_high, crc_high = struct.unpack('<BBB', high_raw)
+    def _get_coefficient(self, command):
+        low = self._get_byte_with_crc_check(command)
+        high = self._get_byte_with_crc_check(command+1)
 
-        if self._calculate_checksum(low<<8|dummy_low) != crc_low \
-           or self._calculate_checksum(high<<8|dummy_high) != crc_high:
+        if low is None or high is None:            
            return None
         return low << 8|high
 
@@ -53,22 +66,47 @@ class HAIGU_DHT22:
         humB = self._get_coefficient(self.CMD_GET_HUMCOEFB_H)
         return humA, humB
 
-    def get_temp_and_humidity(self):
+    def get_temperature_and_humidity(self):
         self._write(self.CMD_MEASURE)
         time.sleep(self.MEASUREMENT_WAIT_TIME)
-
         data = self._read(6)
-        self._debug_print(" RAW temp and humidity", data)
-        temp_data, temp_checksum, humidity_data, humidity_checksum = struct.unpack('>hBhB', data)
+        temp_data, temp_checksum, humidity_data, humidity_checksum = struct.unpack('>HBHB', data)
         
         temp = None
         humi = None
         if self._calculate_checksum(temp_data) == temp_checksum:
-           temp = self._get_temperature(temp_data) 
+           temp = self._compute_temperature(temp_data) 
         if self._calculate_checksum(humidity_data) == humidity_checksum and temp is not None:
-           humi = self._get_humidity(humidity_data, temp)
+           humi = self._compute_humidity(humidity_data, temp)
         return temp, humi
+
+    def _read_integer_response(self):
+        data = self._read(3)
+        raw_int, raw_checksum = struct.unpack('>HB', data)
+        
+        if self._calculate_checksum(raw_int) == raw_checksum:
+           return raw_int
+        return None
+
+    def get_temperature(self):    
+        self._write(self.CMD_MEASURE_TEMPERATURE)
+        time.sleep(self.MEASUREMENT_WAIT_TIME)
+        value = self._read_integer_response()
+        if value is None:
+           return None 
+
+        return self._compute_temperature(value) 
     
+    def get_humidity(self):
+        self._write(self.CMD_MEASURE_HUMIDITY)
+        time.sleep(self.MEASUREMENT_WAIT_TIME)
+        value = self._read_integer_response()
+        if value is None:
+           return None 
+        if self._temperature is None:
+           self.get_temperature()
+        return self._compute_humidity(value) 
+
     def _write(self, value):
         self._debug_print(f"write 0x{self.I2C_ADDRESS:X}:", value)
         self.i2c.writeto(self.I2C_ADDRESS, struct.pack(">H", value), False)
@@ -91,14 +129,12 @@ class HAIGU_DHT22:
         return data
 
     def __enter__(self):
-        """used to enable python's with statement support"""
         return self
 
     def __exit__(self, *exc_info):
         return
 
     def _calculate_checksum(self, value):
-        """Checksum Calculation from an short input"""
         polynomial = 0x131  # //P(x)=x^8+x^5+x^4+1 = 100110001
         crc = 0xFF
         data = struct.pack(">H", value)
@@ -114,25 +150,24 @@ class HAIGU_DHT22:
         self._debug_print('Computed CRC', crc, 'for', value)
         return crc
 
-    def _get_temperature(self, value):
-        """converts  the measured temperature in C by using the following function:
-        T =  40 + (ST/256)
-        where ST is the value from the sensor
-        """
-        return 40.0 + value/256.0
+    def _compute_temperature(self, value):
+        # strip last 4 junky bits
+        value &= 0xFFE0
+        if (value>0x7FF):
+            value -= 0xFFFE
+
+        self._temperature = round(40.0 + value/256.0,1)
+        return self._temperature
     
-    def _get_humidity(self, value, temperature):
-        """RHrel= RH + 0.25*(T - 25.0)"""
-        humA, humB = self.get_humidity_coefficients()
-        if humA is None or humB is None:
-           return None   
-        rh = 30+(((value-humB)*60)/(humA-humB))
-        rel = rh + 0.25 * (temperature - 25.0)
-        if rel > 100:
-           return 100 
-        if rel < 0:
-           return 0 
-        return rel
+    def _compute_humidity(self, value, temperature = None):
+        rh = 30+(((value-self.humB)*60)/(self.humA-self.humB))
+        rel = rh + 0.25 * (self._temperature - 25.0)
+        if rel > 100.0:
+           rel = 100.0 
+        else:
+            if rel < 0.0:
+               rel = 0.0 
+        return int(round(rel,0))
 
     def _debug_print(self, *args):
         if not self.debug:
@@ -163,8 +198,8 @@ import sys
 if __name__ == "__main__":
     try:
         # Adjust pins and frequency if needed
-        i2c = machine.I2C(1, scl=machine.Pin(11), sda=machine.Pin(10), freq=8000)  
-        #i2c = machine.SoftI2C(scl=machine.Pin(11), sda=machine.Pin(10), freq=8000)
+        i2c = machine.I2C(1, scl=machine.Pin(11), sda=machine.Pin(10), freq=1000)  
+        #i2c = machine.SoftI2C(scl=machine.Pin(11), sda=machine.Pin(10), freq=1000)
 
         print('i2c scanning ...')
         devices = i2c.scan()
@@ -176,13 +211,14 @@ if __name__ == "__main__":
           print('i2c devices found:', len(devices))
           for device in devices:
             print("i2c hexadecimal address: ", hex(device))    
-          print('---------------------------------')
         
         with HAIGU_DHT22(i2c, False) as sensor:
             while True:
-                temperature, humidity = sensor.get_temp_and_humidity()
-                print (f"Temperature: {temperature} °C")
-                print (f"Humidity: {humidity} %")
+                print('\n')
+                print (f"Temperature solo:   {sensor.get_temperature()}°C")
+                print (f"Humidity solo:      {sensor.get_humidity()}%")
+                temperature, humidity = sensor.get_temperature_and_humidity()
+                print (f"Temperature & Humidity: {temperature}°C  &  {humidity}%")
                 time.sleep(10)
     except Exception as e:
         print (e)
